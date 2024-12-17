14e5b7406dc20a4aa8e604d48f8187391512ce17
fcrozatier <frederic.crozatier@protonmail.com>
2024-12-17 16:00:55 +0100

    benchmark

---
+++ bench.ts
@@ -0,0 +1,34 @@
+import * as esbuild from "esbuild";
+import tsBlankSpace from "ts-blank-space";
+import ts from "typescript";
+
+const path = "./fixtures.ts";
+console.log(`%cinput ts ${path}`, "color: magenta");
+const code = await Deno.readTextFile(path);
+
+Deno.bench("ts.transpileModule", { baseline: true }, () => {
+  ts.transpileModule(code, {
+    compilerOptions: {
+      target: ts.ScriptTarget.ESNext,
+      module: ts.ModuleKind.ESNext,
+      verbatimModuleSyntax: true,
+      isolatedModules: true,
+      removeComments: true,
+    },
+  });
+});
+
+Deno.bench("tsBlankSpace", () => {
+  tsBlankSpace(code);
+});
+
+Deno.bench("esbuild", async () => {
+  await esbuild.transform(code, {
+    loader: "ts",
+    format: "esm",
+    platform: "browser",
+    target: "esnext",
+    logLevel: "silent",
+    sourcemap: false,
+  });
+});
