aeb976fa384a45cb01b05428ee596a5311c58fcb
fcrozatier <frederic.crozatier@protonmail.com>
2024-12-19 16:12:11 +0100

    add try catch example

--- tests/supported/cases/variable-declaration/input.ts
+++ tests/supported/cases/variable-declaration/input.ts
@@ -6,3 +6,8 @@ const undefinedValue: undefined = undefined;
 const numArray: number[] = [1, 2, 3];
 const strArray: Array<string> = ["a", "b", "c"];
 const mixedTuple: [number, string, boolean] = [42, "tuple", true];
+try {
+  throw new Error("Something went wrong!");
+} catch (error: unknown) {
+  console.error(error);
+}
--- tests/supported/cases/variable-declaration/output.ts
+++ tests/supported/cases/variable-declaration/output.ts
@@ -6,3 +6,9 @@ const undefinedValue = undefined;
 const numArray = [1, 2, 3];
 const strArray = ["a", "b", "c"];
 const mixedTuple = [42, "tuple", true];
+try {
+    throw new Error("Something went wrong!");
+}
+catch (error) {
+    console.error(error);
+}
