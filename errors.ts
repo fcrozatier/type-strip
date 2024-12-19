8a3836ad44fc0e294d56397b61b463aca95d93b8
fcrozatier <frederic.crozatier@protonmail.com>
2024-12-19 14:50:22 +0100

    declare error code

--- errors.ts
+++ errors.ts
@@ -3,6 +3,8 @@ export const ERROR_MESSAGE = {
   "filename": "the fileName must include the extension",
   "extension": "the script must be a TypeScript file",
   // Unsupported features
+  "declare":
+    "Ambiant Declarations are still up for discussion in the TC39 type annotation proposal. At the moment the proposal doesn't reserve space for ambiant declarations",
   "enum":
     "Enums have runtime semantics and are not supported by the TC39 type annotation proposal. An alternative pattern is to use a const object",
   "namespace":
