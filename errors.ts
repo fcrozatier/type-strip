57c2c7e9d48f418574044f52bbdd6d8c97c7eb82
fcrozatier <frederic.crozatier@protonmail.com>
2024-12-20 17:04:59 +0100

    simplify error messages

--- errors.ts
+++ errors.ts
@@ -4,22 +4,18 @@ export const ERROR_MESSAGE = {
   "filename": "the fileName must include the extension",
   // Unsupported features
   "accessor-keyword":
-    "The `accessor` is a TypeScript-only feature with special semantics and is not part of the TC39 type annotation proposal. An alternative is to use explicit getters/setters",
-  "declare":
-    "Ambient Declarations are still up for discussion in the TC39 type annotation proposal. At the moment the proposal doesn't reserve space for ambient declarations",
-  "decorator": "Decorators are not supported",
-  "enum":
-    "Enums have runtime semantics and are not supported by the TC39 type annotation proposal. An alternative pattern is to use a const object",
-  "jsx":
-    "JSX is not intended to be implemented by browsers, but to be used by preprocessors. It's not part of the TC39 proposal.",
-  "overload":
-    "Function overloads are still up for discussion in the TC39 type annotation proposal. At the moment the proposal doesn't reserve space for function overloads.",
-  "namespace":
-    "Namespaces are a legacy TypeScript specific construct to provide modularity and an encapsulation. They are not supported by the TC39 type annotation proposal. Standard ES modules are the preferred way to address these needs",
+    "Auto-Accessors are not supported. Use explicit getters/setters",
+  "declare": "Ambient Declarations are not supported",
+  "decorator":
+    "Decorators are not supported. Use a transpilation step or the mixin pattern",
+  "enum": "Enums are not supported",
+  "jsx": "JSX is not supported",
+  "overload": "Function overloads are not supported",
+  "namespace": "Namespaces are not supported",
   "parameter-property":
-    "Parameter Properties are a convenience TypeScript syntax for turning a constructor parameter into a class property, but do not align with existing Javascript semantics. They are not supported by the TC39 type annotation proposal. The alternative is to use explicit field declarations and assignments",
+    "Parameter Properties are not supported. Use explicit field declarations and assignments",
   "type-assertion-expression":
-    "Prefixed type assertions are discouraged and should be replaced by `as` assertions",
+    "Prefixed type assertions are not supported. Use `as`-expressions",
 };
 
 export class TypeStripError extends Error {
