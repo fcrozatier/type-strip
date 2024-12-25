77b6847b5d37a2f0d7e3d7a0f824ba6caf98a4da
fcrozatier <frederic.crozatier@protonmail.com>
2024-12-25 17:36:24 +0100

    pass an arg for full bench

--- scripts/bench.ts
+++ scripts/bench.ts
@@ -3,37 +3,66 @@ import tsBlankSpace from "ts-blank-space";
 import ts from "typescript";
 import typeStrip from "../index.ts";
 
+/**
+ * Generate a fixture with `Deno task gen:fixtures 2` for ~600 LOC
+ *
+ * Run the benchmark with `Deno task bench` or `Deno task bench -- full`
+ *
+ */
+
 const path = "scripts/fixtures.ts";
 console.log(`%cinput ${path}`, "color: magenta");
-const code = await Deno.readTextFile(path);
+const text = await Deno.readTextFile(path);
 
-Deno.bench("ts.transpileModule", () => {
-  ts.transpileModule(code, {
-    compilerOptions: {
-      target: ts.ScriptTarget.ESNext,
-      module: ts.ModuleKind.ESNext,
-      verbatimModuleSyntax: true,
-      isolatedModules: true,
-      removeComments: true,
+const [kind] = Deno.args;
+
+if (kind === "full") {
+  // Split fixtures by comment sections and test increasing file lengths
+  const sections = text.split(/\/\/.*/gm);
+  sections.reduce((prev, curr) => {
+    const code = prev + curr;
+    run(code);
+    return code;
+  });
+} else {
+  run(text);
+}
+
+function run(code: string) {
+  const lines = code.split(/\n/).length;
+
+  Deno.bench(`ts.transpileModule`, { group: `${lines}` }, () => {
+    ts.transpileModule(code, {
+      compilerOptions: {
+        target: ts.ScriptTarget.ESNext,
+        module: ts.ModuleKind.ESNext,
+        verbatimModuleSyntax: true,
+        isolatedModules: true,
+        removeComments: true,
+      },
+    });
+  });
+
+  Deno.bench(`tsBlankSpace`, { group: `${lines}` }, () => {
+    tsBlankSpace(code);
+  });
+
+  Deno.bench(
+    `type-strip`,
+    { baseline: true, group: `${lines}` },
+    () => {
+      typeStrip(code);
     },
+  );
+
+  Deno.bench(`esbuild`, { group: `${lines}` }, async () => {
+    await esbuild.transform(code, {
+      loader: "ts",
+      format: "esm",
+      platform: "browser",
+      target: "esnext",
+      logLevel: "silent",
+      sourcemap: false,
+    });
   });
-});
-
-Deno.bench("tsBlankSpace", () => {
-  tsBlankSpace(code);
-});
-
-Deno.bench("type-strip", { baseline: true }, () => {
-  typeStrip(code);
-});
-
-Deno.bench("esbuild", async () => {
-  await esbuild.transform(code, {
-    loader: "ts",
-    format: "esm",
-    platform: "browser",
-    target: "esnext",
-    logLevel: "silent",
-    sourcemap: false,
-  });
-});
+}
