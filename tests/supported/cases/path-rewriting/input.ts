f3fa79002a8e9747ff32716839e0fb93c1df93a8
Mark Gibson <mgibson@adaptavist.com>
2025-04-02 23:23:36 +0100

    Add test case for a non-string-literal in a dynamic import

--- tests/supported/cases/path-rewriting/input.ts
+++ tests/supported/cases/path-rewriting/input.ts
@@ -2,4 +2,8 @@ import { a } from "./moduleA.ts";
 import b from "./moduleB.ts";
 export { c } from "./moduleC.ts";
 export * from "./moduleD.ts";
+
+// Dynamic import with a string literal is rewritten
 import("./moduleE.ts");
+// Dynamic import with an expression is not rewritten
+import("./moduleF" + ".ts");
--- tests/supported/cases/path-rewriting/output.js
+++ tests/supported/cases/path-rewriting/output.js
@@ -2,4 +2,8 @@ import { a } from "./moduleA.js";
 import b from "./moduleB.js";
 export { c } from "./moduleC.js";
 export * from "./moduleD.js";
+
+// Dynamic import with a string literal is rewritten
 import("./moduleE.js");
+// Dynamic import with an expression is not rewritten
+import("./moduleF" + ".ts");
