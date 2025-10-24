ad798baca2d543394556ee226f07369f337b2b3c
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2025-10-24 10:16:06 +0200

    fix: remap exports specifiers (#14)

--- tests/supported/cases/option-imports/output.js
+++ tests/supported/cases/option-imports/output.js
@@ -1,3 +1,6 @@
 import a from "../foo/bar.js";
 import { b } from "../fiz/bar.js";
-import { c, } from "@baz/bar";
+import { c,} from "@baz/bar";
+
+export { b } from "../fiz/bar.js";
+export { c, } from "@baz/bar";
